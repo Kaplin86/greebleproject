b554ea1bdbb2d39ae5b04af08e36c34b7186f4ed
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2026-08-09 01:30:23 -0400

    sine background

--- generator.gd
+++ generator.gd
@@ -6,29 +6,55 @@ var IMAGE_SIZE = Vector2i(IMAGE_X,IMAGE_Y)
 
 @export_range(0.1,1,0.02) var wobble = 0.2
 @export_range(0,200,1) var coreSize = 30
+@export_range(0,1,0.01) var frequency = 0.033
+@export_range(1,100,1) var sineThickness = 30
+
+var SEED = "1"
 
 func _generate():
 	var Generated : Image = Image.create(IMAGE_X,IMAGE_Y,false,Image.FORMAT_RGBA8)
 	Generated.fill(Color.WHITE)
 	
+	var noise = FastNoiseLite.new()
+	noise.seed = int(SEED)
+	
 	for X in range(IMAGE_X):
 		for Y in range(IMAGE_Y):
 			
-			Generated.set_pixel(X,Y,getPixelColor(X,Y))
+			Generated.set_pixel(X,Y,getPixelColor(X,Y,noise))
 	
 	return Generated
 
-func getPixelColor(X : int ,Y : int) -> Color:
+func getPixelColor(X : int ,Y : int, noise : FastNoiseLite) -> Color:
+	
 	var color := Color.WHITE
 	
 	var middlepoint = IMAGE_SIZE/2
 	var distMiddlePoint = Vector2i(X,Y).distance_to(middlepoint)
+	
+	# STEP 0 -- centerdistortion
+	if distMiddlePoint >= coreSize + 5 and distMiddlePoint <= coreSize * 2.0:
+		X += 3
+		Y += 3
+	
+	# STEP 1 --- BACKGROUND
+	
+	var xwaveYlevel = (sin(X*frequency) * (IMAGE_X/3.0)) + (IMAGE_X/2)
+	
+	if abs(xwaveYlevel - Y) <= sineThickness and noise.get_noise_2d(X,Y) >= 0.1:
+		color = Color.PURPLE
+	
+	
+	
+	
+	# STEP 2 ----- CORE GENERATION
 	distMiddlePoint -= sin(X+Y * wobble) * (1/max(0.001,wobble))
 	
 	if distMiddlePoint <= coreSize:
 		color = Color.BEIGE
 	elif distMiddlePoint <= coreSize + 5:
 		color = Color.BLACK
+	
 	
 	return color
 
